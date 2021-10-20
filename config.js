24ddb8722ee510dd9345b9a3c7fda1e1d8350190
Đình Hiệp <88882056+toilahiep01@users.noreply.github.com>
2021-10-20 23:17:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const nameGirl = 'Em';
+const nameGirl = 'Mọi người trong CLB Taekwondo Móng Cái';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng mọi người';
